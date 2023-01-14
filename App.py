245113b096df4d6c90793bed9bd182534eba9d61
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-14 17:19:46 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 #First, you'll need to import the necessary libraries, including streamlit, pandas, pandas-profiling, and re:
 import streamlit as st
 import pandas as pd
-import pandas_profiling
+from pandas_profiling import ProfileReport
 import re
 
 #Next, you'll need to create a function to check the file format of the uploaded file. You can use the re library to check if the file name matches the desired file format (e.g. .csv, .xlsx, .xls). If the file format is not valid, you can use Streamlit's st.warning function to display a warning message to the user and ask them to re-upload the file using the desired file format.
@@ -22,7 +22,7 @@ if uploaded_file:
             data = pd.read_excel(uploaded_file)
             
 #After reading the file, you can use the pandas_profiling library to generate a data profiling report. You can use the ProfileReport class to create the report, and then display it using Streamlit's st.write function.
-profile = pandas_profiling.ProfileReport(data)
+profile = ProfileReport(data)
 st.write("Data Profiling Report", profile)
 
 st.write("Dataframe",data)
