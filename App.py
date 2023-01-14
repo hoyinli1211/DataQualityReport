b38e76f05aeca6d2234fd50731443b6924bdd6cd
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-14 17:50:37 +0800

    Update App.py

--- App.py
+++ App.py
@@ -7,20 +7,14 @@ import re
 
 #Next, you'll need to create a function to check the file format of the uploaded file. You can use the re library to check if the file name matches the desired file format (e.g. .csv, .xlsx, .xls). If the file format is not valid, you can use Streamlit's st.warning function to display a warning message to the user and ask them to re-upload the file using the desired file format.
 
-def check_file_format(file):
-    if not re.search(r'\.(csv|xlsx|xls)$', file.name):
-        st.warning("Please upload a file with valid extension. (.csv, .xlsx, .xls)")
-        return False
-    return True
-  
+ 
 #Next, you can use Streamlit's st.file_uploader function to allow the user to upload a file. You can then pass the uploaded file to the check_file_format function to ensure that it's in the desired format. If the file format is valid, you can use the pandas library to read in the file.
 uploaded_file = st.file_uploader("Please upload your file", type=["csv", "xlsx", "xls"])
 if uploaded_file:
-    if check_file_format(uploaded_file):
-        if uploaded_file.name.endswith("csv"):
-            data = pd.read_csv(uploaded_file)
-        elif uploaded_file.name.endswith("xlsx") or uploaded_file.name.endswith("xls"):
-            data = pd.read_excel(uploaded_file)
+    if uploaded_file.name.endswith("csv"):
+        data = pd.read_csv(uploaded_file)
+    elif uploaded_file.name.endswith("xlsx") or uploaded_file.name.endswith("xls"):
+        data = pd.read_excel(uploaded_file)
             
 #After reading the file, you can use the pandas_profiling library to generate a data profiling report. You can use the ProfileReport class to create the report, and then display it using Streamlit's st.write function.
         profile = ProfileReport(data)
