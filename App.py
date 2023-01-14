7c0324273450d4796bcc1fe575320f2691af0a3b
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-14 17:30:26 +0800

    Update App.py

--- App.py
+++ App.py
@@ -1,7 +1,8 @@
 #First, you'll need to import the necessary libraries, including streamlit, pandas, pandas-profiling, and re:
 import streamlit as st
 import pandas as pd
-from pandas_profiling import ProfileReport
+import pandas_profiling
+from streamlit_pandas_profiling import st_profile_report
 import re
 
 #Next, you'll need to create a function to check the file format of the uploaded file. You can use the re library to check if the file name matches the desired file format (e.g. .csv, .xlsx, .xls). If the file format is not valid, you can use Streamlit's st.warning function to display a warning message to the user and ask them to re-upload the file using the desired file format.
