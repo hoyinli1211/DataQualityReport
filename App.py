acd140c56698f0febcc046d595f4898a8941a3af
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-15 09:10:48 +0800

    Update App.py

--- App.py
+++ App.py
@@ -4,6 +4,7 @@ import pandas as pd
 import pandas_profiling
 from pandas_profiling import ProfileReport
 import re
+import matplotlib.pyplot as plt
 
 #Next, you'll need to create a function to check the file format of the uploaded file. You can use the re library to check if the file name matches the desired file format (e.g. .csv, .xlsx, .xls). If the file format is not valid, you can use Streamlit's st.warning function to display a warning message to the user and ask them to re-upload the file using the desired file format.
 
@@ -19,5 +20,5 @@ if uploaded_file:
 #After reading the file, you can use the pandas_profiling library to generate a data profiling report. You can use the ProfileReport class to create the report, and then display it using Streamlit's st.write function.
     profile = ProfileReport(df)
     
-    st.write(profile.to_html(), unsafe_allow_html=True)
+    st.markdown(profile.to_html(), unsafe_allow_html=True)
     st.write("Dataframe",df)
