e62651d53aeaaf1e711970a4996c894cc0c8bcbd
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-15 11:50:56 +0800

    Update App.py

--- App.py
+++ App.py
@@ -5,7 +5,13 @@ import pandas_profiling
 from pandas_profiling import ProfileReport
 import re
 
-
+def introduction():
+    st.title("Introduction to Data Quality Checking")
+    st.markdown("Data quality checking is the process of verifying the completeness, accuracy, consistency, and relevance of data. It is an important step in data preparation and data analysis to ensure that the data is suitable for its intended use.")
+    st.markdown("There are several ways to perform data inspection using Python, including:")
+    st.markdown("- **Manual inspection**: Viewing the data in a spreadsheet or text editor")
+    st.markdown("- **Programmatic inspection**: Using Python libraries such as Pandas, NumPy, and Matplotlib to view and analyze the data")
+    st.markdown("- **Data profiling**: Using libraries such as pandas_profiling, to generate a report that provides an overview of the data including missing values, data types, and statistics.")
 
 # Create a sidebar on the left side of the app
 st.sidebar.title("Instructions")
@@ -22,21 +28,29 @@ st.sidebar.markdown("- The report will show the data profile, missing values, an
 # Define the main part of the app
 st.title("Data Quality Report")
 
-# Allow the user to upload a file
-uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx", "xls"])
+#Multi-page 
+pages = ["Upload File & Generate Report", "Note"]
+selected_page = st.selectbox("Select a page", pages, index=2)
 
-if uploaded_file is not None:
-    if uploaded_file.name.endswith("csv"):
-        df = pd.read_csv(uploaded_file)
-    elif uploaded_file.name.endswith("xlsx") or uploaded_file.name.endswith("xls"):
-        df = pd.read_excel(uploaded_file)
-    
-    # Display the imported file in data frame
-    st.write("Dataframe",df)
-    
-    if st.button('Run Data Quality Check'):
-        # Generate the data profiling report
-        profile = ProfileReport(df)
+if selected_page == "Upload File & Generate Report"
+    # Allow the user to upload a file
+    uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx", "xls"])
 
-        # Display the report in the Streamlit app
-        st.components.v1.html(profile.to_html(), height=2000, scrolling=True)
+    if uploaded_file is not None:
+        if uploaded_file.name.endswith("csv"):
+            df = pd.read_csv(uploaded_file)
+        elif uploaded_file.name.endswith("xlsx") or uploaded_file.name.endswith("xls"):
+            df = pd.read_excel(uploaded_file)
+
+        # Display the imported file in data frame
+        st.write("Dataframe",df)
+
+        if st.button('Run Data Quality Check'):
+            # Generate the data profiling report
+            profile = ProfileReport(df)
+
+            # Display the report in the Streamlit app
+            st.components.v1.html(profile.to_html(), height=2000, scrolling=True)
+
+if selected_page == "Note":
+    introduction()
