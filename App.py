be3ed5f2371515d20b185b082e3cc9103dfba03f
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-15 10:55:17 +0800

    Update App.py

--- App.py
+++ App.py
@@ -4,21 +4,42 @@ import pandas as pd
 import pandas_profiling
 from pandas_profiling import ProfileReport
 import re
-import matplotlib.pyplot as plt
 
-#Next, you'll need to create a function to check the file format of the uploaded file. You can use the re library to check if the file name matches the desired file format (e.g. .csv, .xlsx, .xls). If the file format is not valid, you can use Streamlit's st.warning function to display a warning message to the user and ask them to re-upload the file using the desired file format.
 
- 
-#Next, you can use Streamlit's st.file_uploader function to allow the user to upload a file. You can then pass the uploaded file to the check_file_format function to ensure that it's in the desired format. If the file format is valid, you can use the pandas library to read in the file.
-uploaded_file = st.file_uploader("Please upload your file", type=["csv", "xlsx", "xls"])
-if uploaded_file:
+
+# Create a sidebar on the left side of the app
+st.sidebar.title("Instructions")
+
+# Create a selectbox that allows users to switch between tabs
+tabs = ["Upload File", "Generate Report"]
+selected_tab = st.sidebar.selectbox("Select a tab", tabs)
+
+# Show instructions for the selected tab
+if selected_tab == "Upload File":
+    st.sidebar.markdown("- Click on the 'Upload File' button to select a file from your computer.")
+    st.sidebar.markdown("- Make sure the file is in .csv, .xlsx, .xls format.")
+    st.sidebar.markdown("- Click on the 'Run' button to run the data quality check.")
+
+if selected_tab == "Generate Report":
+    st.sidebar.markdown("- After running the data quality check, the report will be generated.")
+    st.sidebar.markdown("- The report will show the data profile, missing values, and correlation.")
+
+# Define the main part of the app
+st.set_page_config(page_title="Data Quality Report", page_icon=":chart_with_upwards_trend:", layout="wide")
+st.title("Data Quality Report")
+
+# Allow the user to upload a file
+uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx", "xls"])
+
+if uploaded_file is not None:
     if uploaded_file.name.endswith("csv"):
         df = pd.read_csv(uploaded_file)
     elif uploaded_file.name.endswith("xlsx") or uploaded_file.name.endswith("xls"):
         df = pd.read_excel(uploaded_file)
-            
-#After reading the file, you can use the pandas_profiling library to generate a data profiling report. You can use the ProfileReport class to create the report, and then display it using Streamlit's st.write function.
+
+    # Generate the data profiling report
     profile = ProfileReport(df)
-     
+
+    # Display the report in the Streamlit app
     st.write("Dataframe",df)
     st.components.v1.html(profile.to_html(), height=2000, scrolling=True)
