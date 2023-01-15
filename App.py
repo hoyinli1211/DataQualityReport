44786e4273ccb5529b19e0eb3d5f6e90a27a56b2
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-15 09:01:18 +0800

    Update App.py

--- App.py
+++ App.py
@@ -18,7 +18,6 @@ if uploaded_file:
             
 #After reading the file, you can use the pandas_profiling library to generate a data profiling report. You can use the ProfileReport class to create the report, and then display it using Streamlit's st.write function.
     profile = ProfileReport(df)
-    st.pyplot(profile.to_widgets())
-
-    #st.write("Data Profiling Report", profile)
-    st.write("Dataframe",data)
+    
+    st.write(profile.to_html(), unsafe_allow_html=True)
+    st.write("Dataframe",df)
