f67f8a11be24ca245e9f854eb036d564d624b6eb
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-16 23:51:52 +0800

    Update App.py

--- App.py
+++ App.py
@@ -83,7 +83,8 @@ with tab_uploadnreport:
 # Allow the user to upload a file
     st.subheader("Upload File and Generate Report")
     uploaded_file = st.file_uploader("Upload a file", type=["csv", "xlsx", "xls"])
-    if st.button("Import sample dataset"):
+    importbutton = st.button("Import sample dataset"):
+    if importbutton:
         df = load_data("https://raw.githubusercontent.com/hoyinli1211/DataQualityReport/main/loan_sanction_test.csv")
         showdata(df)
         
